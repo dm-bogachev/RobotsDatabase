8449a77264cdd4a5b29038b09c496e1451be83d8
dm-bogachev <dm.bogachev@yandex.ru>
2020-10-03 16:14:50 +0300

    CRUD for robot
    Added full CRUD-system for robot model
    Some html+css changes

--- robots/urls.py
+++ robots/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('integrator/create', IntegratorCreateView.as_view(), name='integrator_create'),
     path('location/create', LocationCreateView.as_view(), name='location_create'),
     path('robot/create', RobotCreateView.as_view(), name='robot_create'),
+    path('robot/read/<int:pk>', RobotReadView.as_view(), name='robot_read'),
+    path('robot/update/<int:pk>', RobotUpdateView.as_view(), name='robot_update'),
+    path('robot/delete/<int:pk>', RobotDeleteView.as_view(), name='robot_delete'),
     path('service/create', ServiceCreateView.as_view(), name='service_create'),
 
 ]
--- robots/views.py
+++ robots/views.py
@@ -5,8 +5,10 @@ from django.http import request
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class HomePageView(TemplateView):
+class HomePageView(LoginRequiredMixin, ListView):
+    model = Robot
     template_name = 'robots/home.html'
+    login_url = 'login'
 
 
 class ArmCreateView(LoginRequiredMixin, CreateView):
@@ -98,6 +100,67 @@ class RobotCreateView(LoginRequiredMixin, CreateView):
     @staticmethod
     def all_integrator():
         return Integrator.objects.all()
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+class RobotReadView(LoginRequiredMixin, DetailView):
+    template_name = 'robots/robot/read.html'
+    model = Robot
+    fields = '__all__'
+    login_url = 'login'
+
+    @staticmethod
+    def all_clients():
+        return Client.objects.all()
+
+    @staticmethod
+    def all_arms():
+        return Arm.objects.all()
+
+    @staticmethod
+    def all_controllers():
+        return Controller.objects.all()
+
+    @staticmethod
+    def all_integrator():
+        return Integrator.objects.all()
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+class RobotUpdateView(LoginRequiredMixin, UpdateView):
+    model = Robot
+    template_name = 'robots/robot/update.html'
+    fields = '__all__'
+    login_url = 'login'
+
+    @staticmethod
+    def all_clients():
+        return Client.objects.all()
+
+    @staticmethod
+    def all_arms():
+        return Arm.objects.all()
+
+    @staticmethod
+    def all_controllers():
+        return Controller.objects.all()
+
+    @staticmethod
+    def all_integrator():
+        return Integrator.objects.all()
+
+    def get_success_url(self):
+        return reverse_lazy('robot_read', kwargs={'pk': self.kwargs['pk']})
+
+
+class RobotDeleteView(LoginRequiredMixin, DeleteView):
+    model = Robot
+    template_name = 'robots/robot/delete.html'
+    login_url = 'login'
 
     def get_success_url(self):
         return reverse_lazy('home')
