1ed4444e7fa2aea8db63f037fa96d8da216cb9b6
dm-bogachev <dm.bogachev@yandex.ru>
2020-10-04 15:22:42 +0300

    Added table search script
    Favicon + tableSearch.js for quick search

--- database/settings.py
+++ database/settings.py
@@ -121,10 +121,7 @@ STATICFILES_DIRS = (os.path.join('static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-#MEDIAFILES_DIRS = (os.path.join('media'),)
-
 AUTH_USER_MODEL = 'users.RobowizardEmployee'
-
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 LANGUAGE_CODE = 'ru-RU'
